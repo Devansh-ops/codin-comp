5714241e465b37cef74dc573ebdfa38a3496215d
Devansh Sehgal <devanshsehgal02@gmail.com>
2021-05-19 23:21:42 +0530

    Create cpp_template.cpp

---
+++ cpp_template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define uli unsigned long int
+using namespace std;
+
+int main(){
+    FAST
+    int t;
+  
+    return 0;
+}
